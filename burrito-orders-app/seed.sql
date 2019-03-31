c14feb2edffb83a53f90fcfb9dddf9a8128ed20b
Anthony Moss <asmoss93@gmail.com>
2019-03-31 19:40:02 -0400

    adds seed file to add data to burrito order table

---
+++ burrito-orders-app/seed.sql
@@ -0,0 +1,7 @@
+insert into burrito_orders(first_name, last_name, type, contents, order_type)
+VALUES
+('Anthony', 'Moss', 'Burrito', 'Chicken, Steak, rice, black beans, queso, guac, lettuce, hot sauce', 'to-go'),
+('Chris', 'Aquino', 'Burrito', 'Chicken, rice, black beans, guac, shredded cheese, pico de gallo, sourcream', 'sit-down'),
+('Kyle', 'Youngkin', 'Bowl', 'Carnitas, rice, black beans, queso, pico de gallo, lettuce, hot sauce', 'to-go'),
+('Oakley', 'Snipes', 'Bowl', 'Steak, carnitas, chicken, rice, queso', 'sit-down'),
+('Sir David', 'Quinith', 'Burrito', 'Steak, chicken, carnitas, rice, black beans, queso, guac, sourcream, pico de gallo, hot sauce', 'to-go');
